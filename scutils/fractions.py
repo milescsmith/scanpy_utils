dfffe680f9bd4d93dab06699f462e277e7db24fc
Miles Smith <miles-smith@omrf.org>
2019-06-21 13:04:46 -0500

    added documentation

--- scutils/fractions.py
+++ scutils/fractions.py
@@ -1,9 +1,30 @@
+from typing import Optional
 import pandas as pd
+from anndata import AnnData
 
 def cluster_fractions(
-    df: pd.DataFrame, numerator: str, denominator: str
+    data_obj: AnnData, numerator: str, denominator: str
 ) -> pd.DataFrame:
+    """Calculate the relative fraction of a population within a given set of groups.
+    More clearly, calculate how many cells of, say, a treatment group make up a 
+    cluster.
 
+    Parameters
+    ----------
+    data_obj : :class:`~anndata.AnnData
+        anndata object with metadata corresponding to the numerator and denominator arguments
+    numerator : `str`
+        Inner group to calculate relative fraction of
+    denominator : `str`
+        Outer group to calculate relative fraction of
+
+    Return
+    ------
+    :class:`~pandas.DataFrame`
+    """
+
+    if isinstance(data_obj, "AnnData"):
+        df = data_obj.obs
     df["index"] = df.index
     beta = df.groupby([numerator, denominator]).count()
     beta = beta.loc[:, ["index"]].rename(columns={"index": "numerator_count"})
