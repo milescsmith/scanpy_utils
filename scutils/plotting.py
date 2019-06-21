42b7866d4f621aa1046635f304e2260dcf027c35
Miles Smith <miles-smith@omrf.org>
2019-06-21 13:04:13 -0500

    removed wraps decorator redundancy

--- scutils/plotting.py
+++ scutils/plotting.py
@@ -1,24 +1,11 @@
 from typing import Union, List
 from matplotlib.axes import Axes
-from scanpy.plotting._tools.scatterplots import plot_scatter
-
-
-def _wraps_plot_scatter(wrapper):
-    annots_orig = {
-        k: v for k, v in wrapper.__annotations__.items() if k not in {"adata", "kwargs"}
-    }
-    annots_scatter = {
-        k: v for k, v in plot_scatter.__annotations__.items() if k != "basis"
-    }
-    wrapper.__annotations__ = {**annots_scatter, **annots_orig}
-    wrapper.__wrapped__ = plot_scatter
-    return wrapper
-
+from scanpy.plotting._tools.scatterplots import plot_scatter, _wraps_plot_scatter
 
 @_wraps_plot_scatter
 def dimplot(adata, reduction="umap", **kwargs) -> Union[Axes, List[Axes], None]:
     """\
-    Scatter plot in UMAP basis.
+    A generalized interface for plotting dimensional reductions.
     Parameters
     ----------
     {adata_color_etc}
