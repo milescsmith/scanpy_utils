25bb12c824d1488c6b8cc45a4fd867a452be9ced
Miles Smith <mileschristiansmith@gmail.com>
2019-09-22 16:27:12 -0500

    remove self import of dimplot

--- scanpy_utils/__init__.py
+++ scanpy_utils/__init__.py
@@ -1,7 +1,6 @@
 from scanpy_utils.fractions import cluster_fractions
 from scanpy_utils.markers import get_marker_genes, top_markers, top_markers_for_group
 from scanpy_utils.plotting import (
-    dimplot,
     velocity_compare,
     plot_grouped_velo_grid,
     plot_dictionary_velo_grid,
